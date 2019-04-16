bb031801b5ebca562e55d7a0dc5f4a1e16d03521
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-04-16 17:51:55 +1200

    Re-format agents.py code

--- dusty_acorn/agents.py
+++ dusty_acorn/agents.py
@@ -16,17 +16,17 @@ class Agent(Thread):
     This is a thread, used as the agent, responsible for listen to events to/from the UI, and interact with message
     queues."""
 
-    def __init__(self, task_queue, result_queue):
+    def __init__(self, tasks_queue, results_queue):
         """Constructor.
 
-        :param task_queue: task queue
-        :type task_queue: Queue
-        :param result_queue: result queue
-        :type result_queue: Queue
+        :param tasks_queue: task queue
+        :type tasks_queue: Queue
+        :param results_queue: result queue
+        :type results_queue: Queue
         """
         Thread.__init__(self)
-        self.taskQ = task_queue
-        self.resultQ = result_queue
+        self.tasks_queue = tasks_queue
+        self.results_queue = results_queue
 
         self.pacman = None
 
@@ -39,13 +39,11 @@ class Agent(Thread):
 
     def close(self):
         """ Deletes any object no needed any more, close connections, etc """
-
-        # self.e.__del__
         self.running = False
         print('agent exited')
 
     def init(self):
-        """ Called only once, in the first run of the agent """
+        """Called once, in the first run of the agent."""
 
         print('Program started.')
         self.send_message_to_ui('Program started.')
@@ -54,23 +52,25 @@ class Agent(Thread):
         self.pacman = Pacman()
 
     def send_message_to_ui(self, message):
-        """ Sends messages to the UI, via the result queue """
+        """Sends messages to the UI, via the result queue."""
 
         data = dict()
         data['message'] = message
         data['type'] = 'message'
-        self.resultQ.put(data)
+        self.results_queue.put(data)
 
     def run(self):
-        """ Contains the main logic of the agent, handling messages received from the UI (taskQ), and sending data back to the UI (resultQ) """
+        """Contains the main logic of the agent.
+
+        Handles messages received from the UI (task_queue), and sends data back to the UI (result_queue)."""
 
         while self.running:
             if not self.has_been_initialized:
                 self.init()
                 self.has_been_initialized = True
             # look for incoming tornado request
-            if not self.taskQ.empty():
-                task = self.taskQ.get()
+            if not self.tasks_queue.empty():
+                task = self.tasks_queue.get()
 
                 # You can view the content of this message in the console where you started the program
                 print("Agent received from web: " + task)
@@ -85,12 +85,11 @@ class Agent(Thread):
 
             # read the serial data
             metrics = self.get_metrics()
-            self.resultQ.put(metrics)
+            self.results_queue.put(metrics)
             time.sleep(1)
 
     def get_metrics(self):
-        """ This method must return a dict, that represents the metrics collected by the agent """
-
+        """This method must return a dict, that represents the metrics collected by the agent."""
         metrics = dict()
 
         metrics['type'] = 'heartbeat'  # message type
