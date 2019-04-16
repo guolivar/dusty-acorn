af7ddcd394b78b99a051dbfa5fa7d8c1d4944f07
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-04-16 17:49:03 +1200

    Re-format main.py code

--- dusty_acorn/main.py
+++ dusty_acorn/main.py
@@ -1,100 +1,100 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+import multiprocessing
 import os
 import sys
-import time
-# Tornardo... HTTP, WebSockets, Events... - pip install tornado
-import tornado.web # pip install tornado
-import tornado.websocket
+
+import tornado.ioloop
+
 from tornado.httpserver import HTTPServer
 from tornado.options import define, options, parse_command_line
-# for queues
-import multiprocessing
-# for the agent
+from tornado.web import Application, RequestHandler
+from tornado.websocket import WebSocketHandler
+
 from .agents import Agent
 
 define("port", default=8080, help="run on the given port", type=int)
 define("debug", default=False, help="run in debug mode")
 
-# Base class, you can add any logic common to all handlers
-class BaseHandler(object):
-    pass
 
-# A handler for Web
-class WebHandler(BaseHandler, tornado.web.RequestHandler):
-    pass
+class IndexHandler(RequestHandler):
 
-# A web socket handler
-class WebSocketHandler(BaseHandler, tornado.websocket.WebSocketHandler):
-    pass
-
-# The implementations. These are the actual handlers, that take care to respond users, 
-# based on the routes that you create for the application.
-
-# /index.html
-class IndexHandler(WebHandler):
-    @tornado.web.asynchronous
     def get(self):
         self.render("index.html", app_name='Air 1')
 
-# /index2.html
-class IndexHandler2(WebHandler):
-    @tornado.web.asynchronous
+
+class IndexHandler2(RequestHandler):
+
     def get(self):
         self.render("index2.html", app_name='Air 1')
 
 
-# /grid.html
-class GridHandler(WebHandler):
-    @tornado.web.asynchronous
+class GridHandler(RequestHandler):
+
     def get(self):
         self.render("grid.html", app_name='Air 1')
 
+
 # /bars.html
-class BarsHandler(WebHandler):
-    @tornado.web.asynchronous
+class BarsHandler(RequestHandler):
+
     def get(self):
         self.render("bars.html", app_name='Air 1')
 
+
 # /distance.html
-class DistanceHandler(WebHandler):
-    @tornado.web.asynchronous
+class DistanceHandler(RequestHandler):
+
     def get(self):
         self.render("distance.html", app_name='Air 1')
-        
+
+
 # /dust.html
-class DustHandler(WebHandler):
-    @tornado.web.asynchronous
+class DustHandler(RequestHandler):
+
     def get(self):
         self.render("dust.html", app_name='Air 1')
 
+
 # /temperature.html
-class TempHandler(WebHandler):
-    @tornado.web.asynchronous
+class TempHandler(RequestHandler):
+
     def get(self):
         self.render("temperature.html", app_name='Air 1')
-class TempQHandler(WebHandler):
-    @tornado.web.asynchronous
+
+
+class TempQHandler(RequestHandler):
+
     def get(self):
         self.render("temperature_qual.html", app_name='Air 1')
+
+
 # /RH.html
-class RhHandler(WebHandler):
-    @tornado.web.asynchronous
+class RhHandler(RequestHandler):
+
     def get(self):
         self.render("rh.html", app_name='Air 1')
+
+
 # /co2.html
-class CO2Handler(WebHandler):
-    @tornado.web.asynchronous
+class CO2Handler(RequestHandler):
+
     def get(self):
         self.render("co2.html", app_name='Air 1')
+
+
 # /co2_rel.html
-class CO2qualHandler(WebHandler):
-    @tornado.web.asynchronous
+class CO2qualHandler(RequestHandler):
+
     def get(self):
         self.render("co2_qual.html", app_name='Air 1')
 
+
 # websockets
 clients = []
+
+
 class MetricsHandler(WebSocketHandler):
     def check_origin(self, origin):
         return True
@@ -120,22 +120,20 @@ class MetricsHandler(WebSocketHandler):
         q = self.application.settings.get('queue')
         q.put(message)
 
-################################ MAIN ################################
-
 
 def main():
-    taskQ = multiprocessing.Queue()
-    resultQ = multiprocessing.Queue()
+    tasks_queue = multiprocessing.Queue()
+    results_queue = multiprocessing.Queue()
 
     parse_command_line()
 
     # here we define the routes that the web app handles
-    app = tornado.web.Application(
-        handlers = [
+    app = Application(
+        handlers=[
             (r"/index2.html", IndexHandler2),
-            (r"/index.html", IndexHandler), 
+            (r"/index.html", IndexHandler),
             (r"/grid.html", GridHandler),
-            (r"/bars.html", BarsHandler),  
+            (r"/bars.html", BarsHandler),
             (r"/distance.html", DistanceHandler),
             (r"/dust.html", DustHandler),
             (r"/temperature.html", TempHandler),
@@ -144,40 +142,40 @@ def main():
             (r"/co2.html", CO2Handler),
             (r"/co2_qual.html", CO2qualHandler),
             (r"/ws", MetricsHandler)
-            ],
+        ],
         cookie_secret="__air_1__",
         template_path=os.path.join(os.path.dirname(__file__), "templates"),
         static_path=os.path.join(os.path.dirname(__file__), "static"),
         debug=options.debug,
-        queue=taskQ
-        )
+        queue=tasks_queue
+    )
 
     # listen to the port and start the app loop and wait for clients
     server = HTTPServer(app)
     server.listen(options.port)
     print("Listening on port:", options.port)
 
-    def checkResults():
-        if not resultQ.empty():
-            result = resultQ.get()
-            #print("tornado received from agent: " + str(result))
+    def check_results():
+        if not results_queue.empty():
+            result = results_queue.get()
+            # print("tornado received from agent: " + str(result))
             for c in clients:
                 c.write_message(result)
 
-    theAgent = Agent(taskQ, resultQ)
-    theAgent.daemon = True
-    mainLoop = tornado.ioloop.IOLoop.instance()
-    scheduler = tornado.ioloop.PeriodicCallback(checkResults, 500, io_loop = mainLoop)
+    the_agent = Agent(tasks_queue, results_queue)
+    the_agent.daemon = True
+    scheduler = tornado.ioloop.PeriodicCallback(check_results, 500)
     scheduler.start()
 
-    theAgent.start()
+    the_agent.start()
+    io_loop = tornado.ioloop.IOLoop.current()
     try:
-        mainLoop.start()
+        io_loop.start()
     except KeyboardInterrupt:
-        taskQ.close()
-        resultQ.close()
-        theAgent.close()
+        tasks_queue.close()
+        results_queue.close()
+        the_agent.close()
         scheduler.stop()
-        mainLoop.stop()
+        io_loop.stop()
     print("Bye!")
     sys.exit(0)
